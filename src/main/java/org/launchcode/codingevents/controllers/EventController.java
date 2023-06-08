a6912af04bffd51d002d8755b918f9614f9a5a19
Kyle Stadler <kstad4@gmail.com>
2023-06-07 22:30:46 -0500

    Completed lesson 14.4

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,9 +28,8 @@ public class EventController {
     //Handler for information submitted using the form in /templates/events/create
     //The request parameter "eventName" is the same name found on the input tag in /templates/events/create
     @PostMapping("create")
-    public String processCreateEvenForm(@RequestParam String eventName,
-                              @RequestParam String eventDescription) {
-        EventData.add(new Event(eventName, eventDescription));
+    public String processCreateEvenForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:/events";
     };
 
