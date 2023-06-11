f317782f0b4be659f44602dc4a98cd410910d8a3
Kyle Stadler <kstad4@gmail.com>
2023-06-11 15:07:06 -0500

    Completed lesson 15.3

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,14 +23,20 @@ public class EventController {
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm() {
       return "events/create";
     };
 
     //Handler for information submitted using the form in /templates/events/create
     //The request parameter "eventName" is the same name found on the input tag in /templates/events/create
     @PostMapping("create")
-    public String processCreateEvenForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
+
         EventData.add(newEvent);
         return "redirect:/events";
     };
